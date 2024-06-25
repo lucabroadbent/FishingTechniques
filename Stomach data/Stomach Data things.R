ec037ab8c4bcc2387250b6a9bcd860d3bdfff103
Luca Broadbent <lekb500@york.ac.uk>
2024-06-25 22:32:40 +0100

    Added the stuff to check if the two clades of mackeral PPMR are due to different data sources (hint, they are not)

--- Stomach data/Stomach Data things.R
+++ Stomach data/Stomach Data things.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(ggplot2)
 library(tidyr)
 #reading in the data
-load("C:/Users/lucab/Downloads/stomach_dataset.Rdata")
+load("C:/Users/lucab/Downloads/stomach_dataset.Rdata") 
 
 anc <- read.csv("C:/Users/lucab/Downloads/AnchovyAndSardine.csv")
 
@@ -18,6 +18,128 @@ murray <- stom_df |>
   rename(w_prey = prey_ind_weight_g, w_pred = pred_weight_g, n_prey = prey_count) |>
   mutate(log_ppmr = log(w_pred / w_prey),
          weight_numbers = n_prey / sum(n_prey))
+
+
+### this section here what i want to do is investigate the spatial distribution of the mackerel data - is it different?
+
+mack <- stom_df%>%
+  filter(pred_species =="Scomber scombrus")
+
+mackgreat12 <- mack%>%
+  filter(ppmr > exp(12))
+
+mackless12 <- mack%>%
+  filter(ppmr < exp(12))
+
+#so now i have the data. i want to find the proportion of each sample (which is the row, i think)
+#that is inside each 
+
+#first, i am going to make sure its a unique sample id (as this makes sure we dont count the same sample twice)
+#i need to do this as i am assuming that the number of samples taken is the effort of sampling, and i hope it is 
+#the same effort in the ranges for each mack dataset.
+
+#what i want here is to only subset the dataframe, and only include sample ids that are unique
+
+mackgreat12 <- mackgreat12%>%
+  distinct(sample_id, ices_rectangle)
+
+mackless12 <- mackless12%>%
+  distinct(sample_id, ices_rectangle)
+
+#now i need to add these up for each rectangle, to find the amount of samples per rectangle
+mackgreat12 <- mackgreat12%>%
+  group_by(ices_rectangle)%>%
+  summarise(n = n())
+
+mackless12 <- mackless12%>%
+  group_by(ices_rectangle)%>%
+  summarise(n = n())
+
+#now i need to normalise these values
+
+mackgreat12 <- mackgreat12%>%
+  mutate(n = n/sum(n))
+
+mackless12 <- mackless12%>%
+  mutate(n = n/sum(n))
+
+#now i want to find the difference between both data sets for a given value of ices rectangle
+#if one dataset doesnt have a value, i want to set it to 0
+
+mackdiff <- full_join(mackgreat12, mackless12, by = "ices_rectangle")%>%
+  mutate(diff = n.x - n.y)
+
+#now i want to plot this
+
+ggplot(mackdiff, aes(x = ices_rectangle, y = diff))+
+  geom_bar(stat = "identity")+
+  xlab("ICES Rectangle")+
+  ylab("Difference in proportion of samples")+
+  ggtitle("Difference in proportion of samples with ppmr > 12 and ppmr < 12")
+
+#ok this shows up something good - in general, the proportion of samples across both of the mack subsets is between 
+#0.5% and 1%, so therefore, they only differ in an effort of 1% of their total samples.
+#however, when I instead look at the difference in number of samples taken from each location, the mack dataset
+#that contains values that are less than 12 has taken many more samples, so across all the rectangles, 
+#there is a large difference. 
+
+#but when it is proportional, there is only a difference of max 1%
+#when i think about it, this is good, as there are not really large differences between the effort allocated
+#to sampling - they have sampled equal amounts in all the rectangles, so the data is comparable.
+
+#however, this variation might be a lot, so i should check the standard deviation of the proportion
+#just to be sure.
+
+#i want to plot the mack great and mack less dataframes as density plots, to see if they are distributed the same
+
+mackgreat12 <- mackgreat12 %>% mutate(group = "ppmr > 12")
+mackless12 <- mackless12 %>% mutate(group = "ppmr < 12")
+
+# Combine the datasets
+combined_data <- bind_rows(mackgreat12, mackless12)
+
+# Plot
+ggplot(combined_data, aes(x = n, color = group)) +
+  geom_density() +
+  scale_color_manual(values = c("ppmr > 12" = "red", "ppmr < 12" = "blue")) +
+  xlab("Proportion of samples") +
+  ylab("Density") +
+  ggtitle("Density plot of proportion of samples with ppmr > 12 and < 12") +
+  theme_minimal()
+ #yes they are.
+#the distribution is majority of rectangles are 0.1% of the samples, and there aren't very many that are above 0.5%
+
+#i want to plot the distribution of the differences between the sampling effort in the rectangles as a boxplot
+
+ggplot(mackdiff, aes(y = diff))+
+  geom_boxplot()+
+  xlab("ICES Rectangle")+
+  ylab("Difference in proportion of samples")+
+  ggtitle("Difference in proportion of samples with ppmr > 12 and ppmr < 12")
+
+#ok now this looks very good now, the 25-75 percentiles are within 0.001 -  which is equal to 0.1% of the samples
+#does this look good? if they on average differ by 0.1%, and they are usually 0.1% anyway, this is a large difference?
+
+#just to check i am going to do it as a percentage change between rectangles of diff samples
+
+mackdiff <- mackdiff%>%
+  mutate(perc_diff = (n.x - n.y)/n.y)
+
+ggplot(mackdiff, aes(x = ices_rectangle, y = perc_diff))+
+  geom_bar(stat = "identity")+
+  xlab("ICES Rectangle")+
+  ylab("Percentage difference in proportion of samples")+
+  ggtitle("Percentage difference in proportion of samples with ppmr > 12 and ppmr < 12")
+#now a quick boxplot
+ggplot(mackdiff, aes(y = perc_diff))+
+  geom_boxplot()+
+  xlab("ICES Rectangle")+
+  ylab("Percentage difference in proportion of samples")+
+  ggtitle("Percentage difference in proportion of samples with ppmr > 12 and ppmr < 12")
+#yep, majority of the differences is between 1% change, so therefore, both samples are taken at the same effort 
+#from both regions.
+
+
 
 
 #both files are loaded in, first lets make them same format
@@ -116,7 +238,6 @@ print(hist_w_prey)
 print(hist_n_prey)
 print(hist_log_ppmr)
 print(hist_weight_numbers)
-
 
 
 
